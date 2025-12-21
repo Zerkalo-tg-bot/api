14ed6a9a82f979850c8dc89ce5c0b8b8035b6fa9
Vladislav Vagner <vladislawwagner@gmail.com>
2025-12-21 06:59:01 +0100

    refactored swagger config

---
+++ src/config/swagger/config.ts
@@ -0,0 +1,9 @@
+import { DocumentBuilder } from "@nestjs/swagger";
+
+export const swaggerConfig = new DocumentBuilder()
+    .setTitle('Zerkalo API Documentation')
+    .setDescription('Zerkalo API Documentation')
+    .setVersion('1.0.0')
+    .addTag("chat", "Chat operations")
+    .addTag("message", "Message operations")
+    .build();
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger';
+import { swaggerConfig } from './config/swagger/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const config = new DocumentBuilder()
-    .setTitle('Zerkalo API Documentation')
-    .setDescription('Zerkalo API Documentation')
-    .setVersion('1.0')
-    .addTag("chat", "Chat operations")
-    .addTag("message", "Message operations")
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
+  
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
